120c4a67c46c567675f92a25131d7f156dc62cc3
Angelica C <angelica.ramos.91@gmail.com>
2020-03-29 18:30:53 -0400

    Update app.js
    Removal of comments, accidentally left in

--- client/app.js
+++ client/app.js
@@ -4,15 +4,9 @@ $('form').submit(function() {
     var person = $('#initials').val();
     var text = $('#message').val();
     var chatEntry = person + ' says: ' + text;
-    // alert(chatEntry);
 
     socket.emit('message', chatEntry);
-
-
-    // socket.emit('message', text);
-
-    // these will reset the input boxes, ready to write new message
-    // $('#initials').val('');
+    
     $('#message').val('');
     return false;
 });
